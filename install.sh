34eb03a619e175e766ed277998be4d6b0fa4a620
Sebastian Marr <mail@sebastianmarr.de>
2014-05-11 18:31:08 +0200

    add simple install script
    The install script simply updates the submodules (for zsh-completion)
    and stows all config files into the home directory.

---
+++ install.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+if ! type "git" > /dev/null; then
+  echo "Please install git first."
+  exit 1
+fi
+
+if ! type "stow" > /dev/null; then
+  echo "Please install stow first."
+  exit 1
+fi
+
+git submodule init
+git submodule update
+stow bash hg tmux zsh git irb vim
